60ca70e34a1533ad06b42d9abe99657f03afa97a
holly.leary <holly.leary@apexlearning.com>
2016-09-11 17:18:42 -0700

    Fleshing out a bit

--- Greetr.js
+++ Greetr.js
@@ -7,9 +7,84 @@
 
 	}
 
-	// Prototype
+	// not exposed. can only use here because it's lexical environment
+	// so Greetr has access, but there is not access outside of here 
+	// because of lexical scope
+	var supportedLangs = ['en', 'es'];
+
+	// don't want developers to change, but want to have acess to
+	var greetings = {
+
+		en: 'Hello',
+		es: 'Hola'
+	};
+
+	var formalGreetings = {
+		en: 'Greetings',
+		es: 'Saludos'
+
+	};
+
+	var logMessages = {
+		en: 'Logged in',
+		es: 'Incio sesion'
+	};
+
+
+	// Prototype - this is one giant single object literal
 	// Where we will put any methods we want used in object that's returned from Greetr 
-	Greetr.prototype = {};
+	// these properties ARE esposed	
+	Greetr.prototype = {
+		fullname: function() {
+			return this.firstname + ' ' this.lastname;
+		}, 
+
+		validate: function() {
+			if (supportedLangs.indexOf(this.language) === -1) {
+				throw "Invalid language";
+			}
+		}, 
+
+		greeting: function() {
+			return greetings[this.language] + ' ' + this.firstname + '!';
+		}, 
+
+		formalGreeting: function() {
+			return formalGreetings[this.language] + ' ' + this.fullname();
+		}, 
+
+		greet: function(formal) {
+			var msg;
+
+			//if undefined or null it will be coerced to 'false'
+			if (formal) {
+				msg = this.formalGreeting();
+			} else {
+				msg = this.greeting()
+			}
+
+			if (console) {
+				console.log(msg);
+			}
+
+			// 'this' refers to the calling object at execution time
+			// makes the method chainable
+			return this;
+		}, 
+
+		log: function() {
+			if (console) {
+				console.log(logMessages[this.language] + ': ' + this.fullName());
+			}
+			return this;
+		}, 
+
+		setLang: function(lang) {
+			this.language = lang;
+			this.validate();
+			return this;
+		}
+	};
 
 	// building a greetr object
 	//any object created from here will point to Greetr.init.prototype for its prototype
--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 var g = G$('John', 'Doe');
+
+g.greet(); // should decide whether you passed formal of not and give an according greeting
+
+// is also chainable, so:
+g.greet().greet(true); // true means yes it is a formal greeting
+
+g.greet().setLang('es').greet(true); //returns formal spanish greeting
+
+g.greet().setLang('fr').greet(true); // throws an error because french is not supported
